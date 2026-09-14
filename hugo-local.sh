a554ef17186e4d4670c93d685af3a6455c8ebd5a
peichins <peichins@users.noreply.github.com>
2026-09-14 14:46:22 +1000

    Updated call out layout

--- hugo-local.sh
+++ hugo-local.sh
@@ -4,4 +4,8 @@ set -a
 . "$(dirname "$0")/.env.local"
 set +a
 
+if [ "${1:-}" = "server" ]; then
+	set -- "$@" -DEF --tlsAuto --disableFastRender --navigateToChanged --poll 700ms --ignoreCache --noHTTPCache
+fi
+
 exec hugo "$@"
